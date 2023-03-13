e8902de352fdc5f78151a4b2cfe08789c1364c25
BananaRama2909 <127746887+BananaRama2909@users.noreply.github.com>
2023-03-13 15:49:15 +0100

    Add files via upload

---
+++ esempio.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int     numero = 69;
+    double  numerolungo = 10000;
+    float   numeroconlavirgola = 7.78;
+    char    carattere = 99;
+
+    printf("il numero è: %d\n", numero);
+    printf("ïl numero lungo è: %f, numero con la virgola è: %f\n", numerolungo, numeroconlavirgola);
+    printf("ïl carattere è %c\n", carattere);
+}
