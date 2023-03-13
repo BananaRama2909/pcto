c3bd353e85d533b5feeb6237f4a2ad42ae55391a
BananaRama2909 <127746887+BananaRama2909@users.noreply.github.com>
2023-03-13 16:48:18 +0100

    Add files via upload

---
+++ miao.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    int y;
+
+    printf("ïnserisci un numero: ");
+    scanf("%d", &x);
+    printf("ïnserisci un altro numero: ");
+    scanf("%d", &y);
+    printf("I numeri inseriti sono x = %d e y = %d\n", x, y);
+}
