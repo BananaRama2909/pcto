2b66fefcae9f9781c30a42f9e7fb2ee2df02e20c
BananaRama2909 <127746887+BananaRama2909@users.noreply.github.com>
2023-03-14 11:24:05 +0100

    Add files via upload

---
+++ consent.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int età = 18;
+
+    if (età < 18)
+    {
+        printf("la condizione è verificata\n");
+    }
+    else
+    {
+        printf("la condizione non è verificata\n");
+    }
+}
