b52fc7ca38f3620c58b468941484f1ccab6b7e84
BananaRama2909 <127746887+BananaRama2909@users.noreply.github.com>
2023-03-14 10:17:01 +0100

    Add files via upload

---
+++ eta.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    char y;
+
+    printf("la mia età è");
+    scanf("%d" , &x);
+    printf("ïo sono");
+    scanf(" %c" , &y);
+    printf("la mia età è %d\n e sono %c\n", x, y);
+
+}
