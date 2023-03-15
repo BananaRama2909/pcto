182b6077baf6227674c993fded5b54e5d86ae383
BananaRama <127746887+BananaRama2909@users.noreply.github.com>
2023-03-15 16:52:55 +0100

    Add files via upload

---
+++ causal.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int i = 0;
+    int x;
+
+    printf("che numero è: ");
+    scanf("%d", &n)
+    int array[n];
+    while(i < n);
+    {
+        scanf("%d", &array[i]);
+
+    }
+}
