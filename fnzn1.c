0c1807bb568bc8e1f9a2e28ee5be75bdaf8c822b
BananaRama <127746887+BananaRama2909@users.noreply.github.com>
2023-03-16 16:54:45 +0100

    Add files via upload

---
+++ fnzn1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int b;
+    int e;
+    int i;
+    int p;
+
+    printf("indica la base: ");
+    scanf("%d", b);
+    printf("indica l'esponente: ");
+    scanf("%d", e);
+
+    while(i < p)
+    {
+        
+    }
+}
